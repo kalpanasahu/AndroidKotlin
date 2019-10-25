066972c19069d58ce9efa1f9f1a7823719c64c57
12kalpana.sahu@gmail.com <kalpana.sahu@impelsys.com>
2019-10-25 13:16:39 +0530

    find my age

--- app/src/main/java/com/kt/androidkotlinstart/MainActivity.kt
+++ app/src/main/java/com/kt/androidkotlinstart/MainActivity.kt
@@ -2,13 +2,10 @@ package com.kt.androidkotlinstart
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        textView.text="Welcome"
     }
 }
