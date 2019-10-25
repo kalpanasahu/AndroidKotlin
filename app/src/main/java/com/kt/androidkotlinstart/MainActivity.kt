eab413528e02411ef23076fd8933c676612250be
12kalpana.sahu@gmail.com <kalpana.sahu@impelsys.com>
2019-10-25 14:06:20 +0530

    tic toc

--- app/src/main/java/com/kt/androidkotlinstart/MainActivity.kt
+++ app/src/main/java/com/kt/androidkotlinstart/MainActivity.kt
@@ -2,23 +2,13 @@ package com.kt.androidkotlinstart
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import kotlinx.android.synthetic.main.activity_findmyage.*
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_findmyage)
-  /*    btnGetAge.setOnClickListener(View.OnClickListener {
-          btnGetAgeClick()
-      })*/
+        setContentView(R.layout.tictoctoy)
+
     }
-    fun btnGetAgeClick(view:View){
-        val userDOB=Integer.parseInt(etDOB.text.toString())
-        val currentYear=Calendar.getInstance().get(Calendar.YEAR)
-        var userAgeInYear=currentYear-userDOB
-        tvShowAge.text="Your Age is $userAgeInYear"
-    }
+
 }
