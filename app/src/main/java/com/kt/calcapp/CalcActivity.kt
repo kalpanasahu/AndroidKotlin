76da4e9707509bf1f8e126e875c9740804006787
12kalpana.sahu@gmail.com <kalpana.sahu@impelsys.com>
2019-11-10 02:50:42 +0530

    design issue fixes

--- app/src/main/java/com/kt/calcapp/CalcActivity.kt
+++ app/src/main/java/com/kt/calcapp/CalcActivity.kt
@@ -2,10 +2,12 @@ package com.kt.calcapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import com.kt.calculator.R
 
-class CalcActivity : AppCompatActivity() {
-
+class CalcActivity :AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calc);
     }
 }
