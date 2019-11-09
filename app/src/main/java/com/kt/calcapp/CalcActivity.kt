ae6de3d2ad4cc7595f49462e02a40f63e934738e
12kalpana.sahu@gmail.com <kalpana.sahu@impelsys.com>
2019-11-10 02:16:09 +0530

    UI design

---
+++ app/src/main/java/com/kt/calcapp/CalcActivity.kt
@@ -0,0 +1,11 @@
+package com.kt.calcapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CalcActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
